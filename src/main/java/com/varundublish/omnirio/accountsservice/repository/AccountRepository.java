f5c0cb5eef46fc458aca333c04078523d68df0a4
vardubs <vardubs@gmail.com>
2020-10-29 02:00:59 +0530

    Initial Working Commit of Account Service

---
+++ src/main/java/com/varundublish/omnirio/accountsservice/repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.varundublish.omnirio.accountsservice.repository;
+
+import com.varundublish.omnirio.accountsservice.model.entities.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+}
